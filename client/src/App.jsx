fddca44d7ad83d89d50e94261d1007dbba51dce0
Neha_Kumari711 <nk8981398@gmail.com>
2024-06-12 12:43:41 +0530

    routes defining

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,16 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-// import { ThemeProvider } from '@material-ui/styles';
-import MainRouter from './MainRouter';
-// import theme from './theme';
-import { Provider } from 'react-redux';
-import { ConfigureStore } from './redux/ConfigureStore.jsx';
+import { Router, Routes } from 'react-router-dom';
 
-const store = ConfigureStore();
+
 
 const App = () => {
     return (
         <Provider store={store}>
-            <BrowserRouter>
+            <Router>
+                <Routes  />
+            </Router>
                 
-                    <MainRouter />
-                
-            </BrowserRouter>
+          
         </Provider>
     )
 }
