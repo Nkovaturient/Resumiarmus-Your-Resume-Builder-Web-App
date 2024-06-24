46cc02710d3fca7df68e2dcbcd9fe27930638011
Neha_Kumari711 <nk8981398@gmail.com>
2024-06-24 21:06:41 +0530

    build fixes

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,40 +1,53 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import inject from '@rollup/plugin-inject'
-import { NodeGlobalsPolyfillPlugin} from '@esbuild-plugins/node-globals-polyfill';
-import {NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    inject({
-      util: 'util',
-      //inject Buffer as global variable
-      // Buffer:['buffer', 'Buffer'],
-      process: 'process'
-    }),
-  ],
-  optimizeDeps: {
-    esbuildOptions:{
-      define: {
-        global: 'globalThis'
-      },
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          buffer: true
-        }),
-        NodeModulesPolyfillPlugin()
-      ]
-    }
-  },
-  resolve: {
-    alias: {
-      util: 'rollup-plugin-node-builtins/polyfills/util',
-      stream: 'rollup-plugin-node-builtins/polyfills/stream',
-      // buffer: 'rollup-plugin-node-builtins/polyfills/buffer',
-      process: 'process/browser'
+  plugins: [react()],
+})
 
-    }
-  }
-});
+
+
+
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import inject from '@rollup/plugin-inject'
+// import { NodeGlobalsPolyfillPlugin} from '@esbuild-plugins/node-globals-polyfill';
+// import {NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     inject({
+//       util: 'util',
+//       //inject Buffer as global variable
+//       // Buffer:['buffer', 'Buffer'],
+//       process: 'process'
+//     }),
+//   ],
+//   optimizeDeps: {
+//     esbuildOptions:{
+//       define: {
+//         global: 'globalThis'
+//       },
+//       plugins: [
+//         NodeGlobalsPolyfillPlugin({
+//           buffer: true
+//         }),
+//         NodeModulesPolyfillPlugin()
+//       ]
+//     }
+//   },
+//   resolve: {
+//     alias: {
+//       util: 'rollup-plugin-node-builtins/polyfills/util',
+//       stream: 'rollup-plugin-node-builtins/polyfills/stream',
+//       // buffer: 'rollup-plugin-node-builtins/polyfills/buffer',
+//       process: 'process/browser'
+
+//     }
+//   }
+// });
